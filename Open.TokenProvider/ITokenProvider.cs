ee41c689333cd205cf13c7c72cda153e24fa6254
electricessence <5899455+electricessence@users.noreply.github.com>
2022-03-31 22:02:23 -0700

    Revision.

--- Open.TokenProvider/ITokenProvider.cs
+++ Open.TokenProvider/ITokenProvider.cs
@@ -5,13 +5,34 @@ namespace Open.TokenProvider;
 
 public interface ITokenProvider<TToken>
 {
-	Task<IToken<TToken>> Token { get; }
+	Task<IToken<TToken>> CurrentToken { get; }
 
 	Task<IToken<TToken>> Refresh(DateTimeOffset timestamp);
 }
 
 public static class TokenProviderExtensions
 {
+	public static async Task Attempt<TToken>(
+	this ITokenProvider<TToken> provider,
+	Func<TToken, Task> handler)
+	{
+		if (provider is null) throw new ArgumentNullException(nameof(provider));
+		if (handler is null) throw new ArgumentNullException(nameof(handler));
+
+		var token = await provider.CurrentToken;
+
+		try
+		{
+			await handler(token.Value);
+		}
+		catch
+		{
+			token = await provider.Refresh(token.Timestamp);
+			// Try 1 extra time because the curren token might have been stale.
+			await handler(token.Value);
+		}
+	}
+
 	public static async Task<T> Attempt<TToken, T>(
 		this ITokenProvider<TToken> provider,
 		Func<TToken, Task<T>> handler)
@@ -19,7 +40,7 @@ public static class TokenProviderExtensions
 		if (provider is null) throw new ArgumentNullException(nameof(provider));
 		if (handler is null) throw new ArgumentNullException(nameof(handler));
 
-		var token = await provider.Token;
+		var token = await provider.CurrentToken;
 
 		try
 		{
